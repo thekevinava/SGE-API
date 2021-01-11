8f227cba9ddd3545b2caba2bcc06ac6d3a84243a
Kevinava <kevinava1996@gmail.com>
2021-01-11 20:40:24 +0100

    asd

---
+++ server/src/index.ts
@@ -0,0 +1,40 @@
+import express, { Application } from 'express';
+
+import morgan from 'morgan';
+import cors from 'cors';
+
+class Server {
+
+    public app: Application;
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+    /**
+     * Configuro el servidor y el puerto
+     */
+    config(): void {
+        this.app.set('port', process.env.PORT || 3000); // Configuro el puerto
+        this.app.use(morgan('dev'));
+        this.app.use(cors());
+        this.app.use(express.json()); // Trabajar con JSON
+        this.app.use(express.urlencoded({ extended: false })); // Trabajar con URLs
+    }
+
+    /**
+     * Rutas de nuestra API
+     */
+    routes(): void {
+
+    }
+
+    start(): void {
+        this.app.listen(this.app.get('port'), () => {
+            console.log('Servidor escuchando en el puerto', this.app.get('port'));
+        });
+    }
+}
+
+const server = new Server();
+server.start();
